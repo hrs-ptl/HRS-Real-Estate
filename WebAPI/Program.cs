e8599974a2b37789c9e4a3928f02cb5d4332db6b
Harsh <pateh63@mcmaster.ca>
2023-09-28 17:32:22 -0400

    Create DB and fetch data using EF core -code first

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
+
 
 var app = builder.Build();
 
